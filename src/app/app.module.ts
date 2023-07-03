c995ecf82172d33f5e87c9bdc74f38612b2d9e67
Pravalikapi <121709052+Pravalikapi@users.noreply.github.com>
2023-07-03 11:48:12 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,20 @@ import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MaincontentComponent } from './maincontent/maincontent.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const routes: Routes = [
+  { path: '', component: MaincontentComponent },
+  { path: 'conversion', component: CurrencyrateComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  
+
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,11 +43,13 @@ import { FooterComponent } from './footer/footer.component';
     AdminComponent,
     NavbarComponent,
     MaincontentComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,FormsModule
+    AppRoutingModule,FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
